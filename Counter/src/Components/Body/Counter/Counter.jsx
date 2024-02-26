b0b7bd0638aeadbf0b418604b2da4d6f9dafb6e7
Santiago Cuadros Mateus <79917786+sacuadros09@users.noreply.github.com>
2024-02-26 12:22:46 -0500

    Delete Counter directory

--- Counter/src/Components/Body/Counter/Counter.jsx
+++
@@ -1,5 +0,0 @@
-export const Counter = ({countNumber}) => {
-    return (
-        <h3 className="counter">{countNumber}</h3>
-    )
-}
--- Counter/src/Components/Footer/Footer.jsx
+++
@@ -1,7 +0,0 @@
-
-
-export const Footer = () => {
-    return (
-      <p>By Santiago Cuadros</p>
-    )
-  }
