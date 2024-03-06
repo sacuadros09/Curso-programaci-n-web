a5dea78101559f22861b04bd6746c54026bced2a
Santiago Cuadros Mateus <79917786+sacuadros09@users.noreply.github.com>
2024-03-06 18:21:35 -0500

    Delete clase directory

--- clase/src/data/OverwatchData.js
+++
@@ -1,8 +0,0 @@
-export const OverwatchNavData = [
-    {
-      img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Overwatch_circle_logo.svg/600px-Overwatch_circle_logo.svg.png',
-      Title: 'Overwatch 2',
-      Subtitle: "Acción por equipos"
-    },
-
-]
