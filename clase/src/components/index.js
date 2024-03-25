dd6f5f192e201dbbebe38bdc8ba8b4c3545770c5
Santiago Cuadros Mateus <79917786+sacuadros09@users.noreply.github.com>
2024-03-24 23:45:48 -0500

    Delete clase directory

--- clase/src/components/index.js
+++
@@ -1,9 +0,0 @@
-export * from "../components/Body"
-export * from "../components/ButtonInfo"
-export * from "../components/Catalogue"
-export * from "../components/Contacts"
-export * from "../components/DownloadBattle"
-export * from "../components/Footer"
-export * from "../components/Games"
-export * from "../components/Header"
-
--- clase/src/data/HeroesData.js
+++
@@ -1,8 +0,0 @@
-export const HeroesNavData = [
-    {
-      img: 'https://kbin.social/media/cache/resolve/post_thumb/33/35/3335569b567bdf30e1c9f74af904a1a595abcd8858569d0e9c491d7c249386f3.png',
-      Title: 'Heroes of the Storm',
-      Subtitle: "MOBA"
-    },
-
-]
